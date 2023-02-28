1ac8a59dbb0ff6c31834ba145ee898118a900a15
DuongCuc <duongcuc131@gmail.com>
2023-02-28 16:16:53 +0700

    upload task 3

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # KCSC Train
+a
--- task_2/logout.php
+++ task_2/logout.php
@@ -3,10 +3,3 @@
 session_start();
 session_destroy();
 header('location: login.php');
-=======
-<?php
-session_start();
-session_destroy();
-header('location: login.php');
->>>>>>> 75d38a7bfd679592cc53d9ec800162156a842400
-?>
