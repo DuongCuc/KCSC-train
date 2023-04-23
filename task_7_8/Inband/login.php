c102b1f621ec890fc5738918674b39dc7381508d
DuongCuc <duongcuc131@gmail.com>
2023-04-23 22:26:28 +0700

    rename

---
+++ task_7_8/Inband/login.php
@@ -0,0 +1,45 @@
+<?php
+    session_start()
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Đăng nhập</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form action="login.php" method="POST">
+            Tên đăng nhập: <input type="text" name="username">
+            <br/>
+            Mật khẩu: <input type="password" name="password">
+            <br/>
+            <input type="submit" class="button" name="login" value="Đăng nhập">
+            <input type="submit" class="button" name="register" value="Đăng ký">
+        </form>
+        <?php
+        $connect=mysqli_connect("localhost","root","","kcsc");
+        if(isset($_POST['login'])){
+            $username=$_POST['username'];
+            $password=$_POST['password'];
+            if($username==""||$password==""){
+                echo 'Điền đầy đủ thông tin';
+            }else{
+                $password=md5($password);
+                $query= mysqli_query($connect, "SELECT * FROM `task7` WHERE username='$username'AND password='$password'");
+                if(mysqli_num_rows($query)!=0){
+                    $rows=mysqli_fetch_assoc($query);
+                    $_SESSION['id']=$rows['id'];
+                    $_SESSION['username']=$rows['username'];
+                    $_SESSION['password']=$rows['password'];
+                    header('location: index.php');
+                }else{
+                    echo 'Tên đăng nhập hoặc mật khẩu sai.';
+                }
+            }
+        }
+        if(isset($_POST['register'])){
+            header('location:register.php');
+        }
+        ?>
+    </body>
+</html>
